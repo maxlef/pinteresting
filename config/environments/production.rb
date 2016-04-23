22edd2c65b85ba82dd023a54aae68c01d251032b
Maxence Lefrancois <maxence.lefrancois@gmail.com>
2016-04-23 00:49:38 +0000

    added devise gem for auth

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  
+  #required for heroku and devise auth
+  config.action_mailer.default_url_options = { host: 'omr-max-pinteresting.com', port: 3000 }
 end
